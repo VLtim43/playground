7a7b7cbd0f9c98dbe6027c70fe0adc97b89cec47
VLtim43 <fernandoreal.valentim@gmail.com>
2026-09-20 21:21:18 -0300

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+// terraform init: setup project and download providers
+// terraform fmt: format files
+// terraform plan: preview AWS changes
+// terraform apply: create or update AWS resources
+// terraform show: show current state
+// terraform destroy: delete all resources created
+
 terraform {
   required_providers {
     aws = {
